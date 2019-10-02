36075af4b0df877545f479a3ba5cea957ed57f9f
Melvin Wong <melvin.wong@ryerson.ca>
2019-10-02 10:15:19 -0400

    new linear regression model

---
+++ genome/models/linear.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""A simple Linear regression model"""
+from genome.models import base
+from genome.models import functions as fn
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+FLOATX = theano.config.floatX
+
+
+class LinearRegression(base.BaseModel):
+    """
+    Initializes a Linear Regression model
+
+    This class creates a generic linear regression model: y = Wx + b.
+
+    Args:
+        input: A symbolic ``Tensor`` input
+        n_vars (int): Number of input variables
+        W: Slope parameters - A ``TensorSharedVariable`` (optional)
+        b: Intercept parameter - A ``TensorSharedVariable`` (optional)
+
+    """
+    def __init__(self, input, n_vars, W=None, b=None):
+        super().__init__(input)
+
+        if W is None:
+            init = np.zeros((n_vars, 1), dtype=FLOATX)
+            W = theano.shared(value=init, name='W', borrow=True)
+        self.W = W
+
+        if b is None:
+            init = np.zeros((1,), dtype=FLOATX)
+            b = theano.shared(value=init, name='b', borrow=True)
+        self.b = b
+
+        self.params = [self.W, self.b]
+        self.params_m = [None, None]
+
+        self.output = T.dot(input, self.W) + self.b
+
+    def mean_squared_error(self, y):
+        return fn.mean_squared_error(self, y)
